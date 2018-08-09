985daeb303e48473eac910ac6bb04591ed07ff02
pengnengsong <pengnengsong@outlook.com>
2018-08-09 18:02:57 +0800

    将jedis交给spring管理

---
+++ ZuulServer/src/main/java/com/gateway/utils/RedisConfig.java
@@ -0,0 +1,62 @@
+package com.gateway.utils;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+/**
+ * Created by pengns on 2018/8/9.
+ */
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    private static final Logger logger= LoggerFactory.getLogger(RedisConfig.class);
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.redis.pool")
+    public JedisPoolConfig getRedisConfig(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        return config;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.redis")
+    public JedisConnectionFactory getConnectionFactory(){
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        JedisPoolConfig config = getRedisConfig();
+        factory.setPoolConfig(config);
+        logger.info("JedisConnectionFactory bean init success.");
+        return factory;
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate redisTemplate() {
+        JedisConnectionFactory jedisConnectionFactory = getConnectionFactory();
+        StringRedisTemplate template = new StringRedisTemplate(jedisConnectionFactory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
