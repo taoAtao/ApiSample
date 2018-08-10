b01463bd3d34c15b1596398e922ec9dc935b8e89
xiangt <xiangtao1994@hotmail.com>
2018-08-10 14:05:20 +0800

    加入日志配置

--- AlarmService/src/main/java/com/example/AlarmApplication.java
+++ AlarmService/src/main/java/com/example/AlarmApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ProviderApplication {
+public class AlarmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProviderApplication.class, args);
+		SpringApplication.run(AlarmApplication.class, args);
 	}
 }
